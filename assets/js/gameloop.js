fd9f4a10e32dc916baf13160ee74ae8460a18ce3
sdeseille <sebastien.deseille@gmail.com>
2024-08-19 16:16:09 +0200

    removed initial sprite from the tutorial

--- assets/js/gameloop.js
+++ assets/js/gameloop.js
@@ -71,30 +71,14 @@ for (let card_color in first_line_cards){
 }
 console.log(game_cards)
 
-let sprite = Sprite({
-  x: 100,        // starting x,y position of the sprite
-  y: 80,
-  color: 'red',  // fill color of the sprite rectangle
-  width: 20,     // width and height of the sprite rectangle
-  height: 40,
-  dx: 2          // move the sprite 2px to the right every frame
-});
-
 let loop = GameLoop({  // create the main game loop
   update: function() { // update the game state
-    sprite.update();
-    // wrap the sprites position when it reaches
-    // the edge of the screen
-    if (sprite.x > canvas.width) {
-      sprite.x = -sprite.width;
-    }
     for (card in game_cards){
       //console.log(card);
       game_cards[card].update();
     }
   },
   render: function() { // render the game state
-    sprite.render();
     for (card in game_cards){
       //console.log(card);
       game_cards[card].render();
