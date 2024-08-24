53bbe4424576a02765166f8a48e35cb175c0714e
sdeseille <sebastien.deseille@gmail.com>
2024-08-24 13:45:34 +0200

    Added pointer events handling. Card are revealed when clicked on.

--- assets/js/gameloop.js
+++ assets/js/gameloop.js
@@ -1,12 +1,17 @@
-let { init, Sprite, Text, GameLoop } = kontra
-let { canvas } = init();
+let { init, Sprite, Text, GameLoop, track, initPointer, initInput } = kontra
+// , onDown, onPointerDown, collidesWithPointer, onInput, pointer
+let { canvas, context } = init();
+// this function must be called first before input
+// functions will work
+initInput();
+initPointer();
 
 const card_colours = ['heart', 'diamond', 'club', 'spade'];
 
 let full_deck = {};
 
 let gameboard_height = 3;
-let gameboard_width  = 4;
+let gameboard_width  = 5;
 
 
 let card_figures = {
@@ -58,47 +63,79 @@ let generate_random = function(){
 }
 let picked_cards = generate_random();
 console.log(picked_cards);
-// let second_line_cards = generate_random();
-// let third_line_cards  = generate_random();
-// let fourth_line_cards = generate_random();
 console.log(game_deck);
 
 let game_cards = [];
 let card_pos_x = 10;
 let card_pos_y = 10;
-let card_width = (canvas.width / gameboard_width) - 20;
+let card_width = (canvas.width / gameboard_width) - 12;
 let card_height = (canvas.height / gameboard_height) - 20;
 
 class Card {
   constructor(x, y, width, height, color, card_color, card_figure) {
     this.card_color = card_color;
     this.card_figure = card_figure;
+    this.returned = false;
     this.sprite = Sprite({
       x: x,
       y: y,
       width: width,
       height: height,
-      color: color
+      color: color,
+      onDown: this.onPointerDown.bind(this)
     });
 
     this.text = Text({
       text: capitalize_first_letter(card_figure) + '\n\n' + card_color,
-      font: '18px Arial',
+      font: '16px Arial',
       color: 'black',
       x: x + width / 2,
       y: y + height / 2,
       anchor: { x: 0.5, y: 0.5 },
-      textAlign: 'center'
+      textAlign: 'center',
+      onDown: this.onPointerDown.bind(this)
     });
+    // Track pointer events on this sprite
+    track(this.sprite, this.text);
   }
 
-  render() {
-    this.sprite.render();
-    this.text.render();
+  show_card(){
+    this.returned=true;
   }
+
+  update(){
+    this.sprite.update();
+    this.text.update();
+  }
+
+  render(){
+    if (this.returned){
+      this.sprite.render();
+      this.text.render();
+    } else {
+      this.text.render();
+      this.sprite.render();
+    }
+  }
+
+  // Handle the pointer down event
+  onPointerDown() {
+    console.log(`Card with text "${this.text.text}" clicked!`);
+    // Additional actions on click can be added here
+    this.show_card();
+    console.log(game_cards)
+  }
+
+  // Check if the card was clicked
+  checkForClick() {
+    if (this.sprite.collidesWithPointer()) {
+      this.onPointerDown();
+    }
+  }
+
 }
 
-let cards_by_line_limit = 4;
+let cards_by_line_limit = gameboard_width;
 let cards_by_line = 0;
 let card_X = card_pos_x;
 let card_Y = card_pos_y;
@@ -118,6 +155,9 @@ for (let card_color in picked_cards){
       cards_by_line = 0;
     }
     let card_symbol = new Card(card_X, card_Y, card_width, card_height, 'white', card_figures[card_color], picked_cards[card_color][card]);
+
+    card_symbol.render();
+    //track(card_symbol.text);
     game_cards.push(card_symbol);
     cards_by_line += 1;
   }
@@ -145,12 +185,17 @@ let text2 = Text({
   textAlign: 'center'
 });
 
+/* // Register the pointer event listener to check all cards
+pointer.onDown(function() {
+  game_cards.forEach(card => game_cards[card].checkForClick());
+}); */
+
 let loop = GameLoop({  // create the main game loop
   update: function() { // update the game state
-/*     for (card in game_cards){
-      console.log(card);
-      //game_cards[card].update();
-    } */
+    for (card in game_cards){
+      //console.log(card);
+      game_cards[card].update();
+    }
   },
   render: function() { // render the game state
     for (card in game_cards){
@@ -161,5 +206,6 @@ let loop = GameLoop({  // create the main game loop
     // text2.render();
   }
 });
+
 
 loop.start();    // start the game
